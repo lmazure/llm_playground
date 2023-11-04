03a756deb4eb93f4e8b4e81dfab44ad859a7c0cd
Laurent <laurent@gilgamesh>
2023-11-04 15:39:38 +0100

    Generation of a HTML report

--- hugging-face.py
+++ hugging-face.py
@@ -1,13 +1,15 @@
 from dotenv import load_dotenv, find_dotenv
 import os
 import requests
+import json
+from html import escape
 
 load_dotenv(find_dotenv())
 token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
-model ="HuggingFaceH4/zephyr-7b-beta" # https://huggingface.co/HuggingFaceH4/zephyr-7b-beta
+model ="HuggingFaceH4/zephyr-7b-beta" # see https://huggingface.co/HuggingFaceH4/zephyr-7b-beta
 
-API_URL = "https://api-inference.huggingface.co/models/" + model
+API_URL = "https://api-inference.huggingface.co/models/" + model # see https://huggingface.co/docs/api-inference/detailed_parameters
 
 def query(payload, token):
     headers = {"Authorization": f"Bearer {token}"}
@@ -39,6 +41,44 @@ payload = {
     }
 }
 
-data = query(payload, token)
+#data = query(payload, token)
+#print(data[0]['generated_text'])
 
-print(data[0]['generated_text'])
+with open('requirements.json', 'r') as file:
+    spec = json.load(file)
+
+def convert_string_to_html(str):
+    s = escape(str)
+    return s.replace('\n', '<br>')
+
+def convert_to_html(spec):
+    html = f"<h1>{convert_string_to_html(spec['title'])}</h1>\n"
+    html += "<ul>\n"
+    for requirement in spec['requirements']:
+        html += f"<li>{convert_string_to_html(requirement)}</li>\n"
+    if 'parts' in spec:
+        for part in spec['parts']:
+            html += f"<li>"
+            html += convert_to_html(part)
+            html += f"</li>"
+    html += "</ul>\n"
+    return html
+
+def generate_html(spec):
+    html = "<html>\n"
+    html += "<head>\n"
+    html += "<title>My Page</title>\n"
+    html += "</head>\n"
+    html += "<body>\n"
+    html += convert_to_html(spec)
+    html += "</body>\n"
+    html += "</html>\n"
+    return html
+
+html_content = generate_html(spec)
+
+# Write the HTML content to a file
+with open('data.html', 'w') as f:
+    f.write(html_content)
+
+os.startfile('data.html')
