39f531a0a32aeb1f9d9d07e733048cf4069877ef
Laurent <laurent@gilgamesh>
2023-11-22 20:11:38 +0100

    A minimum of structure for the JS code

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
 /**
  * Manages the resizable page panels.
  */
-function manageResizeables() {
+function manageResizablePanels() {
     let drag;
     let first, second;
 
@@ -25,12 +25,130 @@ function manageResizeables() {
     }
 }
 
+
+
+// -----------------------------------------------------------
+// SPECIFICATION MANAGEMENT
+// -----------------------------------------------------------
+
+// Inserts a specification into a DOM node.
+
+// Parameters:
+// - node: The DOM node into which the specification will be inserted.
+// - specification: The specification object that contains the title, requirements, and parts.
+// - incr: The current value of the increment used to assign unique IDs to checkboxes.
+
+// Returns:
+// - The updated value of incr after inserting the specification.
+function insertSpecification(node, specification, incr) {
+    const h1 = document.createElement('h1');
+    h1.textContent = specification['title'];
+    node.appendChild(h1);
+    const ul = document.createElement('ul');
+    node.appendChild(ul);
+    for (let i = 0; i < specification['requirements'].length; i++) {
+        const requirement = specification['requirements'][i];
+        const li = document.createElement('li');
+        ul.appendChild(li);
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = incr;
+        li.appendChild(checkbox);
+        const requirement_id = document.createTextNode(requirement['id'] + " - ");
+        li.appendChild(requirement_id);
+        const requirement_text_lines = requirement['text'].split('\n');
+        // Iterate over the lines and create text nodes and <br> elements
+        for (let j = 0; j < requirement_text_lines.length; j++) {
+            li.appendChild(document.createTextNode(requirement_text_lines[j]));
+            if (j < (requirement_text_lines.length - 1)) {
+                li.appendChild(document.createElement('br'));
+            }
+        }
+        incr += 1;
+    }
+    if ('parts' in specification) {
+        for (let i = 0; i < specification['parts'].length; i++) {
+            const part = specification['parts'][i];
+            const li = document.createElement('li');
+            ul.appendChild(li);
+            incr = insertSpecification(li, part, incr);
+        }
+    }
+    return incr;
+}
+
+// Inserts a specification into the DOM.
+
+// Parameters:
+// - specification: The specification to be inserted.
+
+// Return: None.
+function insertSpecificationBlock(specification) {
+    const specificationElement = document.getElementById('specification');
+    const form = document.createElement('form');
+    specificationElement.appendChild(form);
+    insertSpecification(form, specification, 0);
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('onclick', 'generateTestCases()');
+    button.textContent = 'Generate test cases';
+    form.appendChild(button);
+}
+
+function loadSpecification() {
+        const request = new XMLHttpRequest();
+        request.open('GET', 'http://127.0.0.1:5000/specification');
+        request.onreadystatechange = function () {
+            if ((request.readyState === 4) && (request.status === 200)) {
+                insertSpecificationBlock(JSON.parse(request.responseText));
+            }
+        };
+        request.send();
+}
+
+
+// -----------------------------------------------------------
+// MODEL PARAMETER MANAGEMENT
+// -----------------------------------------------------------
+
+function insertModelParameters(parameters) {
+    const parametersElement = document.getElementById('specification');
+    const h1 = document.createElement('h1');
+    h1.textContent = parameters['Model parameters'];
+    parametersElement.appendChild(h1);
+    const ul = document.createElement('ul');
+    parametersElement.appendChild(ul);
+    for (let i = 0; i < parameters['fields'].length; i++) {
+        const field = parameters['fields'][i];
+        const li = document.createElement('li');
+        ul.appendChild(li);
+        const name = document.createTextNode(field['name']);
+        li.appendChild(name);
+    }
+}
+
+function loadModelParameters() {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://127.0.0.1:5000/parameters');
+    request.onreadystatechange = function () {
+        if ((request.readyState === 4) && (request.status === 200)) {
+            insertModelParameters(JSON.parse(request.responseText));
+        }
+    };
+ }
+
+
+// -----------------------------------------------------------
+// TEST CASE MANAGEMENT
+// -----------------------------------------------------------
+
 // Inserts a block of test cases into the 'test-cases' element.
 // 
 // Parameters:
 // - testCases: An array of test cases to be inserted.
 // 
 // Returns: None.
+
 function insertTestCasesBlock(testCases) {
     const testCasesElement = document.getElementById('test-cases');
     const ul = document.createElement('ul');
@@ -82,88 +200,15 @@ function generateTestCases() {
     request.send('selectedItems=' + encodeURIComponent(JSON.stringify(selectedItems)));
 }
 
-// Inserts a specification into a DOM node.
 
-// Parameters:
-// - node: The DOM node into which the specification will be inserted.
-// - specification: The specification object that contains the title, requirements, and parts.
-// - incr: The current value of the increment used to assign unique IDs to checkboxes.
-
-// Returns:
-// - The updated value of incr after inserting the specification.
-function insertSpecification(node, specification, incr) {
-    const h1 = document.createElement('h1');
-    h1.textContent = specification['title'];
-    node.appendChild(h1);
-    const ul = document.createElement('ul');
-    node.appendChild(ul);
-    for (let i = 0; i < specification['requirements'].length; i++) {
-        const requirement = specification['requirements'][i];
-        const li = document.createElement('li');
-        ul.appendChild(li);
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.id = incr;
-        li.appendChild(checkbox);
-        const requirement_id = document.createTextNode(requirement['id'] + " - ");
-        li.appendChild(requirement_id);
-        const requirement_text_lines = requirement['text'].split('\n');
-        // Iterate over the lines and create text nodes and <br> elements
-        for (let j = 0; j < requirement_text_lines.length; j++) {
-            li.appendChild(document.createTextNode(requirement_text_lines[j]));
-            if (j < (requirement_text_lines.length - 1)) {
-                li.appendChild(document.createElement('br'));
-            }
-        }
-        incr += 1;
-    }
-    if ('parts' in specification) {
-        for (let i = 0; i < specification['parts'].length; i++) {
-            const part = specification['parts'][i];
-            const li = document.createElement('li');
-            ul.appendChild(li);
-            incr = insertSpecification(li, part, incr);
-        }
-    }
-    return incr;
-}
-
-function insertModelParameters(parameters) {
-    const parametersElement = document.getElementById('specification');
-    const h1 = document.createElement('h1');
-    h1.textContent = parameters['Model parameters'];
-    parametersElement.appendChild(h1);
-    const ul = document.createElement('ul');
-    parametersElement.appendChild(ul);
-    for (let i = 0; i < parameters['fields'].length; i++) {
-        const field = parameters['fields'][i];
-        const li = document.createElement('li');
-        ul.appendChild(li);
-        const name = document.createTextNode(field['name']);
-        li.appendChild(name);
-    }
-}
-
-// Inserts a specification into the DOM.
-
-// Parameters:
-// - specification: The specification to be inserted.
-
-// Return: None.
-function insertSpecificationBlock(specification) {
-    const specificationElement = document.getElementById('specification');
-    const form = document.createElement('form');
-    specificationElement.appendChild(form);
-    insertSpecification(form, specification, 0);
-    const button = document.createElement('button');
-    button.setAttribute('type', 'button');
-    button.setAttribute('onclick', 'generateTestCases()');
-    button.textContent = 'Generate test cases';
-    form.appendChild(button);
-}
-
+// -----------------------------------------------------------
+// LOGS MANAGEMENT
+// -----------------------------------------------------------
 
 // Add a method that is called every 500 ms and adds text at the end of the DOM.
+
+
+
 lastLogIndex = -1;
 function manageLogs() {
     setInterval(function () {
@@ -193,30 +238,15 @@ function manageLogs() {
     }, 1000);
 }
 
-// retrieve and display the specification when page loads
-window.addEventListener('load', function () {
-    const request = new XMLHttpRequest();
-    request.open('GET', 'http://127.0.0.1:5000/specification');
-    request.onreadystatechange = function () {
-        if ((request.readyState === 4) && (request.status === 200)) {
-            insertSpecificationBlock(JSON.parse(request.responseText));
-        }
-    };
-    request.send();
-});
 
-// retrieve and display the model parameters when page loads
-window.addEventListener('load', function () {
-    const request = new XMLHttpRequest();
-    request.open('GET', 'http://127.0.0.1:5000/parameters');
-    request.onreadystatechange = function () {
-        if ((request.readyState === 4) && (request.status === 200)) {
-            insertModelParameters(JSON.parse(request.responseText));
-        }
-    };
-    request.send();
 
+// -----------------------------------------------------------
+// LET'S START!
+// -----------------------------------------------------------
+
+window.addEventListener('load', function () {
+    manageResizablePanels();
+    loadSpecification();
+    loadModelParameters();
     manageLogs();
-
-    manageResizeables();
 });
